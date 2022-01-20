7a1e8897f99d8946db5f92adbf24abda229db6d8
zjw <1677465766@qq.com>
2022-01-20 20:07:13 +0800

    完成事项:1.按需导入vant组件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Button, Loading, Area } from 'vant';
 let Vue = createApp(App);
+
+Vue.use(Button);
+Vue.use(Loading);
+Vue.use(Area);
+
+
 Vue.mount('#app')
