4d5e3b7043688cf2fc6162cde7c1bf6bf74ab530
syz247179876 <247179876@qq.com>
2022-01-21 15:27:36 +0800

    完成事项: 使用另一种方式实现输入框clear的功能

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,7 +1,57 @@
 <template>
-    <div>推荐页面</div>
+    <div>推荐页面
+        <van-form ref="form">
+            <van-field
+                v-model="state.username"
+                placeholder="请输入手机号码"
+                :right-icon="state.closeUsername"
+                @click-right-icon="closeUsername"
+            ></van-field>
+             <van-field
+                v-model="state.password"
+                placeholder="请输入手机号码"
+                :right-icon="state.closePassword"
+                @click-right-icon="closePassword"
+            ></van-field>
+        </van-form>
+    </div>
 </template>
-<script></script>
+<script>
+import { reactive, watch } from 'vue'
+
+export default {
+    name: 'Recommend',
+    setup() {
+        const state = reactive({
+            username: '',
+            password: '',
+            closeUsername: '',
+            closePassword: ''
+        });
+        const closeUsername = (e) => {
+            console.log(e)
+            state.username = '';
+        };
+        const closePassword = (e) => {
+            console.log(e)
+            state.password = '';
+        };
+        watch(() => [state.username, state.password], () => {
+            if (state.username != '')
+                state.closeUsername = 'close';
+            else
+                state.closeUsername = '';
+            
+            if (state.password != '')
+                state.closePassword = 'close';
+            else
+                state.closePassword = '';
+        });
+        return { state, closeUsername, closePassword };
+    },
+    
+}
+</script>
 
 <style lang="less" scoped>
 
