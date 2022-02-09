9f5e672fdd78f4fa650f85efe437ffe4f4612763
syz247179876 <247179876@qq.com>
2022-02-09 13:56:11 +0800

    feat(注册): 完善用户注册的组件，联调网易云的相关API。

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { Button, Form, Field} from 'vant';
+import { Button, Form, Field, Notify} from 'vant';
 import router from './router'
 import '@/assets/css/base.css'
+import "@/common/global";
 let Vue = createApp(App);
 
+// vant组件全局注册
 Vue.use(Button);
 Vue.use(Form);
 Vue.use(Field);
+Vue.use(Notify);
 
 Vue.use(router);
 
